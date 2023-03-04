c22e0d299ff47d580051d70a5b78f8f773aa79ce
Isabel0715 <1512114230@qq.com>
2023-03-03 20:00:26 -0800

    Modify test.sh for schdule and add tesst-base.sh to remove some sanitizers.

--- benchmark/schedule/test.sh
+++ benchmark/schedule/test.sh
@@ -10,64 +10,56 @@ export TIMEOUT="-k 0.5 0.5"
 export LOG=$BENCHMARK_DIR/log.txt
 export INDIR=$BENCHMARK_DIR/n10train
 
-source $CHISEL_BENCHMARK_HOME/benchmark/test-base.sh
+source $BENCHMARK_DIR/test-base.sh
 
 function clean() {
-  rm -f $REDUCED_BIN $LOG log.txt
+  rm -f $REDUCED_BIN $LOG temp
   return 0
 }
 
 function desired() {
   #echo 0
   { timeout $TIMEOUT $REDUCED_BIN 2 5 3 < $INDIR/dat541 >&$LOG; } 
-  $ORIGIN_BIN 2 5 3 < $INDIR/dat541 >&log.txt
-  diff -q $LOG log.txt >&/dev/null || exit 1
-  # cat log.txt
+  $ORIGIN_BIN 2 5 3 < $INDIR/dat541 >&temp
+  diff -q $LOG temp >&/dev/null || exit 1
   #echo 1
   { timeout $TIMEOUT $REDUCED_BIN 0 4 0 < $INDIR/tc.22  >&$LOG; } 
-  $ORIGIN_BIN 0 4 0 < $INDIR/tc.22 >&log.txt
-  diff -q $LOG log.txt >&/dev/null || exit 1
-  # cat log.txt
+  $ORIGIN_BIN 0 4 0 < $INDIR/tc.22 >&temp
+  diff -q $LOG temp >&/dev/null || exit 1
   #echo 2
   { timeout $TIMEOUT $REDUCED_BIN 0 0 0 < $INDIR/tc.99 >&$LOG ;} 
-  $ORIGIN_BIN  0 0 0 < $INDIR/tc.99 >&log.txt
-  diff -q $LOG log.txt >&/dev/null || exit 1
-  # cat log.txt
+  $ORIGIN_BIN  0 0 0 < $INDIR/tc.99 >&temp
+  diff -q $LOG temp >&/dev/null || exit 1
+  # cat temp
   #echo 3
   { timeout $TIMEOUT $REDUCED_BIN 5 0 4 < $INDIR/dat217  >&$LOG  ;} 
-  $ORIGIN_BIN 5 0 4 < $INDIR/dat217  >&log.txt
-  diff -q $LOG log.txt >&/dev/null || exit 1
-  # cat log.txt
+  $ORIGIN_BIN 5 0 4 < $INDIR/dat217  >&temp
+  diff -q $LOG temp >&/dev/null || exit 1
   #echo 4
   { timeout $TIMEOUT $REDUCED_BIN 10  9  8  < $INDIR/lu56    >&$LOG ;}
-  $ORIGIN_BIN 10  9  8  < $INDIR/lu56  >&log.txt
-  diff -q $LOG log.txt >&/dev/null || exit 1
-  # cat log.txt
+  $ORIGIN_BIN 10  9  8  < $INDIR/lu56  >&temp
+  diff -q $LOG temp >&/dev/null || exit 1
+  # cat temp
   #echo 5
   { timeout $TIMEOUT $REDUCED_BIN 5  4  9  < $INDIR/lu159   >&$LOG  ;} 
-  $ORIGIN_BIN 5  4  9  < $INDIR/lu159  >&log.txt
-  diff -q $LOG log.txt >&/dev/null || exit 1 
-  # cat log.txt
+  $ORIGIN_BIN 5  4  9  < $INDIR/lu159  >&temp
+  diff -q $LOG temp >&/dev/null || exit 1 
   #echo 6
   { timeout $TIMEOUT $REDUCED_BIN 3 4 1 < $INDIR/tc.65  >&$LOG  ;}  
-  $ORIGIN_BIN 3 4 1 < $INDIR/tc.65  >&log.txt
-  diff -q $LOG log.txt >&/dev/null || exit 1
-  # cat log.txt 
+  $ORIGIN_BIN 3 4 1 < $INDIR/tc.65  >&temp
+  diff -q $LOG temp >&/dev/null || exit 1
   #echo 7
   { timeout $TIMEOUT $REDUCED_BIN 1 1 < $INDIR/ad.2 >&$LOG    ;} 
-  $ORIGIN_BIN 1 1 < $INDIR/ad.2  >&log.txt
-  diff -q $LOG log.txt >&/dev/null || exit 1
-  # cat log.txt
+  $ORIGIN_BIN 1 1 < $INDIR/ad.2  >&temp
+  diff -q $LOG temp >&/dev/null || exit 1
   #echo 8
   { timeout $TIMEOUT $REDUCED_BIN 2 5 1 < $INDIR/dat028  >&$LOG  ;} 
-  $ORIGIN_BIN 2 5 1 < $INDIR/dat028  >&log.txt
-  diff -q $LOG log.txt >&/dev/null || exit 1
-  # cat log.txt
+  $ORIGIN_BIN 2 5 1 < $INDIR/dat028  >&temp
+  diff -q $LOG temp >&/dev/null || exit 1
   #echo 9
   { timeout $TIMEOUT $REDUCED_BIN 1  6  7  < $INDIR/lu295  >&$LOG ;}  
-  $ORIGIN_BIN 1  6  7  < $INDIR/lu295 >&log.txt
-  diff -q $LOG log.txt >&/dev/null || exit 1
-  # cat log.txt
+  $ORIGIN_BIN 1  6  7  < $INDIR/lu295 >&temp
+  diff -q $LOG temp >&/dev/null || exit 1
   return 0
 
 }
